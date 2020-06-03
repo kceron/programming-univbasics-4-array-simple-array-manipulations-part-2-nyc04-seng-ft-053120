a0e73b4a797bd564d1fa4fd0fd04a9e74058f552
Karem Ceron <karemiceron@gmail.com>
2020-06-03 02:01:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,9 +3,7 @@ def using_concat(array1, array2)
 end 
 
 def using_insert(array, new_elem)
-    
+  array.insert(4, new_elem)    
 end 
 
-a = %w{ a b c d }
-a.insert(2, 99)         #=> ["a", "b", 99, "c", "d"]
-a.insert(-2, 1, 2, 3)  
+ 
